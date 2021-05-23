3f33b4a05deaa6f344b74a210ee9a86e07666fd2
unknown <luciano.miller.f@gmail.com>
2021-05-23 17:46:02 -0300

    deploy!

--- Front/wisboo-front/src/components/Navbar.jsx
+++ Front/wisboo-front/src/components/Navbar.jsx
@@ -8,14 +8,14 @@ function Navbar() {
         <div>
             <nav class="navbar navbar-expand-lg navbar-dark " style={{backgroundColor:"#2EA0B2", padding:"10px"}}>
                 <div class="container-fluid">
-                <Link to={"/"} style={{marginLeft:"5%"}}><img  src={logo}  alt="logo" /></Link> 
+                <Link to={"/"} style={{marginLeft:"5%",marginRight:"55%"}}><img  src={logo}  alt="logo" /></Link> 
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav" style={{marginLeft:"75%"}}>                        
+                    <div class="navbar-nav" style={{marginLeft:"20%"}}>                        
                         <Link to={"/"} style={{textDecoration:"none",fontSize:"30px", marginRight:"20px"}}><a class="nav-link " href="#">Search</a></Link>
-                        <Link to={"/myimages"} style={{textDecoration:"none",fontSize:"30px"}}><a class="nav-link " href="#">My Images</a></Link>
+                        <Link to={"/myimages"} style={{textDecoration:"none",fontSize:"30px" }}><a class="nav-link " href="#">My Images</a></Link>
                         
                     </div>
                     </div>
